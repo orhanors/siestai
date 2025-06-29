1090435a06f3288ed87e191ca421df3f8d64aad3
cryptorhan <orhan.ors@cryptobooks.tax>
2025-06-29 14:13:04 +0300

    move training files to another project

--- app/main.py
+++ app/main.py
@@ -1,22 +1,14 @@
 from typing import TypedDict
 from typing_extensions import Annotated
 from langgraph.graph.message import add_messages
-from llm.brain import Brain, LLM_MODEL
-from tools.tools import BrainTools
-from langgraph.graph import StateGraph, START, END
+from llm.brain import LLM_MODEL
+from rawdata.intercom import IntercomDataProvider
 
 #CHOOSE THE BASE MODEL
 LLM_MODEL = LLM_MODEL.GROQ_LLAMA3_1_8B_INSTANT
 class AgentState(TypedDict):
     messages: Annotated[list, add_messages]
 
-#Define the tools
-tools = BrainTools()
-tools.add_search_tool()
-
-#CREATE THE BRAIN with TOOLS
-brain = Brain(LLM_MODEL, "generic_agent")
-brain.add_tools(tools=tools.get_tools())
-
-
-graph = StateGraph(AgentState)
+intercom_data_provider = IntercomDataProvider()
+articles, next_page = intercom_data_provider.get_articles(per_page=10, page=33)
+print("next_page::", next_page)
